10e813339a23378c25221d156caf848b8440e295
Bolarinwa Ahmed <bolarinwaahmed22@gmail.com>
2024-05-05 06:48:24 +0100

    Added credentials true to cors setup

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const { cloudinaryConfig } = require("./utils/cloudinary");
 const app = express();
 
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
